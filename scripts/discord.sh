40c833220cf8cad9e403ad610414f8c3525ed02a
justchokingaround <ivanonarch@tutanota.com>
2022-11-07 12:38:04 +0100

    lf

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -3,6 +3,7 @@
 token=$(cat ~/dox/credentials/discord_token.txt)
 launcher="tofi --require-match false --fuzzy-match true --prompt-text > "
 # launcher="fzf"
+base="https://discord.com/api/v10"
 
 nth() {
   stdin=$(cat)
@@ -10,13 +11,13 @@ nth() {
   [ -n "$line" ] && echo "$stdin"|sed "${line}q;d" || exit 1
 }
 
-server_choice=$(curl -s -H "Authorization: $token" "https://discord.com/api/v9/users/@me/guilds"|tr "{|}" "\n"|
+server_choice=$(curl -s -H "Authorization: $token" "${base}/users/@me/guilds"|tr "{|}" "\n"|
   sed -nE "s@\"id\": \"([0-9]*)\", \"name\": \"([^\"]*)\".*@\1\t\2@p"|nth "\$2")
 [ -z "$server_choice" ] && exit 1
 server_name=$(printf "%s" "$server_choice"|cut -f2)
 server_id=$(printf "%s" "$server_choice"|cut -f1)
 
-channel=$(curl -s -H "Authorization: $token" "https://discord.com/api/v9/guilds/${server_id}/channels"|tr "{|}" "\n"|
+channel=$(curl -s -H "Authorization: $token" "${base}/guilds/${server_id}/channels"|tr "{|}" "\n"|
   sed -nE "s@\"id\": \"([0-9]*)\".*\"type\": 0, \"name\": \"([^\"]*)\", \"position\": ([0-9]*).*@\3) \2\t\1@p"|sort -h|cut -f2 -d' ')
 channel_choice=$(printf "%s" "$channel"|nth "\$1")
 [ -z "$channel_choice" ] && exit 1
