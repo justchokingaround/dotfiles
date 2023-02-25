e36e5f2da03d00837f8198292d8fd4320ed08e6b
justchokingaround <ivanonarch@tutanota.com>
2023-02-25 19:38:20 +0100

    oxocarbon rofi

--- hypr/scripts/autostart.sh
+++ hypr/scripts/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 hyprctl dispatch exec "footclient -T spotify_player spotify_player"
-hyprctl keyword windowrule "workspace 1 silent,Electron" && hyprctl dispatch exec "webcord"
+#hyprctl keyword windowrule "workspace 1 silent,Electron" && hyprctl dispatch exec "webcord"
+hyprctl keyword windowrule "workspace 1 silent,Electron" && hyprctl dispatch exec "discord"
 sleep 15
 hyprctl keyword windowrule "unset,Electron"
