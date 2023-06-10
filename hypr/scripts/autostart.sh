737061c70e8927699dea9a4c70394f682415be8e
justchokingaround <ivanonarch@tutanota.com>
2023-06-10 02:25:24 +0200

    hypr stuff

--- hypr/scripts/autostart.sh
+++ hypr/scripts/autostart.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-hyprctl dispatch exec "wezterm start --class spotify_player spotify_player"
+# hyprctl dispatch exec "wezterm start --class music zellij --layout ~/.config/zellij/layouts/music.kdl"
 #hyprctl keyword windowrule "workspace 1 silent,Electron" && hyprctl dispatch exec "webcord"
-hyprctl keyword windowrule "workspace 1 silent,Electron" && hyprctl dispatch exec "discord"
-sleep 15
-hyprctl keyword windowrule "unset,Electron"
+# hyprctl keyword windowrule "workspace 1 silent,Electron" && hyprctl dispatch exec "discord"
+# sleep 15
+# hyprctl keyword windowrule "unset,Electron"
+# hyprctl dispatch exec "cool-retro-term -T iamb -e iamb"
+# hyprctl dispatch exec "cool-retro-term -T discordo -e discordo --token ~/dox/credentials/discord_token.txt"
